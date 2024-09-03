3e240561d101e2ac8ec281bfd28d59ae87d55301
adelbert00 <tazor1337@gmail.com>
2024-09-03 17:30:03 +0200

    FE_SETUP: Prettier

--- SooundBy/resources/js/Pages/Home.vue
+++ SooundBy/resources/js/Pages/Home.vue
@@ -1,28 +1,28 @@
 <template>
-    <TopBar />
-    <div class="text-center flex flex-col items-center section">
-      <div id="Me">
-        <h1
-          class="xl:text-5xl sm:text-4xl pt-20 font-bold text-shadow-custom-header"
-        >
-          Wojciech Faber
-        </h1>
-        <p
-          class="pb-3 xl:text-lg sm:text-base text-black font-mono font-bold text-shadow-custom-p"
-        >
-          Sound designer & Mix/master engineer
-        </p>
-      </div>
-      <img
-        :src="imageSrc"
-        alt="My Photo"
-        class="rounded-full xl:w-2/5 sm:w-4/5 md:w-3/5 shadow-custom-black"
-      />
+  <TopBar />
+  <div class="section flex flex-col items-center text-center">
+    <div id="Me">
+      <h1
+        class="text-shadow-custom-header pt-20 font-bold sm:text-4xl xl:text-5xl"
+      >
+        Wojciech Faber
+      </h1>
+      <p
+        class="text-shadow-custom-p pb-3 font-mono font-bold text-black sm:text-base xl:text-lg"
+      >
+        Sound designer & Mix/master engineer
+      </p>
     </div>
-  </template>
-  
-  <script setup lang="ts">
+    <img
+      :src="imageSrc"
+      alt="My Photo"
+      class="rounded-full shadow-custom-black sm:w-4/5 md:w-3/5 xl:w-2/5"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
 import TopBar from '../Screens/TopBar.vue';
-  
-  const imageSrc = new URL("../assets/photos/prot.jpg", import.meta.url).href;
-  </script>
+
+const imageSrc = new URL('../assets/photos/prot.jpg', import.meta.url).href;
+</script>
--- SooundBy/resources/js/Screens/TopBar.vue
+++ SooundBy/resources/js/Screens/TopBar.vue
@@ -1,18 +1,20 @@
 <template>
   <nav class="bg-transparent">
-    <div class="h-16 flex items-center">
-      <div class="text-black w-full">
+    <div class="flex h-16 items-center">
+      <div class="w-full text-black">
         <ul
-          class="flex font-normal justify-center xl:gap-20 sm:gap-5 sm:text-base md:text-2xl font-custom text-shadow-custom-p"
+          class="text-shadow-custom-p flex justify-center font-custom font-normal sm:gap-5 sm:text-base md:text-2xl xl:gap-20"
         >
           <li
             v-for="section in navigationSections"
             :key="section.id"
-            class="hover:text-indigo-600 animate-fadeDownDelay transition-all hover:scale-125"
+            class="animate-fadeDownDelay transition-all hover:scale-125 hover:text-indigo-600"
           >
-            <a href="#" @click="() => handleClick(section.id)">{{
-              section.name
-            }}</a>
+            <a
+              href="#"
+              @click="() => handleClick(section.id)"
+              >{{ section.name }}</a
+            >
           </li>
         </ul>
       </div>
@@ -22,8 +24,8 @@
 </template>
 
 <script setup lang="ts">
-import { navigationSections } from "../constants/navigation.ts";
-import { scrollToSection } from "../constants/scroll-to.ts";
+import { navigationSections } from '../constants/navigation.ts';
+import { scrollToSection } from '../constants/scroll-to.ts';
 
 const handleClick = (sectionId: string) => {
   scrollToSection(sectionId);
